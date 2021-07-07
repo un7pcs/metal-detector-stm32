9386c95271cc595ee22a7003e00c181d8feeab38
Yanis Baysagov <36517098+un7pcs@users.noreply.github.com>
2021-07-08 01:02:15 +0600

    DMA setup

--- Core/Src/core.c
+++ Core/Src/core.c
@@ -1,6 +1,11 @@
 #include "display.h"
 #include "main.h"
+#include "stm32f1xx_ll_dma.h"
 #include "stm32f1xx_ll_tim.h"
+
+#define ADC_DATA_LENGTH 850
+
+int16_t adc_buffer[ADC_DATA_LENGTH];
 
 void main_task()
 {
@@ -16,7 +21,11 @@ void main_task()
     LL_TIM_EnableCounter(TIM3);
     LL_TIM_EnableAllOutputs(TIM3);
 
-    LL_ADC_
+    LL_DMA_SetDataLength(DMA1, LL_DMA_CHANNEL_1, ADC_DATA_LENGTH);
+    LL_DMA_SetPeriphAddress(DMA1, LL_DMA_CHANNEL_1, LL_ADC_DMA_GetRegAddr(ADC1, LL_ADC_DMA_REG_REGULAR_DATA));
+    LL_DMA_SetMemoryAddress(DMA1, LL_DMA_CHANNEL_1, (uint32_t)adc_buffer);
+
+    LL_DMA_EnableChannel(DMA1, LL_DMA_CHANNEL_1);
 
     display_init();
 }
